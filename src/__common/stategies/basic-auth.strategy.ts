245b81f591c7b797b78ef70c62e0f4d4a401ff45
adentdk <adentrisnadaudkurnia@gmail.com>
2023-06-21 13:59:21 +0700

    fix: remove unused variable

--- src/__common/stategies/basic-auth.strategy.ts
+++ src/__common/stategies/basic-auth.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { BasicStrategy } from 'passport-http';
 import { UsersService } from 'src/users/users.service';
