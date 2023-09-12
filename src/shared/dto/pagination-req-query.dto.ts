a1e3c4649ee877d5f652ed97affa359740ad2d6f
adentdk <adentrisnadaudkurnia@gmail.com>
2023-09-12 09:05:31 +0700

    add sort & sortby

--- src/shared/dto/pagination-req-query.dto.ts
+++ src/shared/dto/pagination-req-query.dto.ts
@@ -1,12 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsIn, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class PaginationReqQueryDto {
   @ApiProperty({ required: false })
+  @IsOptional()
+  @IsNumber()
   page?: number;
 
   @ApiProperty({ required: false })
+  @IsOptional()
+  @IsNumber()
   pageSize?: number;
 
   @ApiProperty({ required: false })
+  @IsOptional()
+  @IsIn(['asc', 'desc'])
+  order?: 'asc' | 'desc';
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  orderBy?: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
   search?: string;
 }
