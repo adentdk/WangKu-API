469f37a21f21a3faca25cd022adeaa58e0e0b5a6
adentdk <adentrisnadaudkurnia@gmail.com>
2023-09-14 16:24:52 +0700

    update

--- src/modules/transactions/transactions.module.ts
+++ src/modules/transactions/transactions.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 import { TransactionsController } from './transactions.controller';
+import { Transaction } from './transactions.entity';
 import { TransactionsService } from './transactions.service';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Transaction])],
   controllers: [TransactionsController],
   providers: [TransactionsService],
 })
